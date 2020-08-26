691e4b924a3df76ebb366865c88e297381e518b9
michael thomas <michael.thomas2014@gmail.com>
2020-08-26 15:10:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
   //add static methods here
+  
+  static capitalize(str){
+    str.charAt(0) = str.charAt(0).toUpperCase();
+  }
+  
+  static sanitize(str){
+    
+  }
+  
+  static titleize(str){
+    
+  }
+  
+  
+  
 }
